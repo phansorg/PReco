6fcb7359606c99d33e2075103b21f28aa466c2dc
phansorg <phan_games@hotmail.com>
2022-08-13 17:18:36 +0900

    json設定ファイル追加、作業ディレクトリをTargetDirに変更

--- preco/preco.cpp
+++ preco/preco.cpp
@@ -1,10 +1,35 @@
-﻿// preco.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include <iostream>
+#include <fstream>
+#include <filesystem>
 
-#include <iostream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+nlohmann::json settings;
+const char* SETTINGS_FILE_NAME = "settings.json";
+const char* LOGS_DIRECTORY = "logs_directory";
+
+void initSettings() {
+
+    if (std::filesystem::exists(SETTINGS_FILE_NAME)) {
+        std::ifstream ifs(SETTINGS_FILE_NAME);
+        settings = json::parse(ifs);
+        return;
+    }
+    
+    settings[LOGS_DIRECTORY] = "./logs";
+
+    std::ofstream ofs(SETTINGS_FILE_NAME);
+    ofs << std::setw(4) << settings << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    initSettings();
+    std::cout << settings[LOGS_DIRECTORY].dump(4) << std::endl;
+
+    std::cout << "Hello World!" << std::endl;
 }
+
+
 
