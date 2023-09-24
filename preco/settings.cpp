3b3a2597c2340cfef86baa4b72a700eaa3e77bcc
phansorg <phan_games@hotmail.com>
2023-09-24 11:26:25 +0900

    座標のコメントを追加

--- preco/settings.cpp
+++ preco/settings.cpp
@@ -37,16 +37,30 @@ void Settings::init()
     json_["log_path"] = "D:/puyo/movie/logs/log.txt";
 
     json_["player_history_dir"] = "D:/puyo/movie/history";
+
+	// ぷよの左端
     json_["player_field_x"] = { 186, 837 };
+    // フィールドの上端
     json_["player_field_y"] = 106;
+    // ぷよの右端まで
     json_["player_field_w"] = 258;
+    // フィールドの下端まで
     json_["player_field_h"] = 480;
-    json_["player_nxt1_x"] = { 479, 757 };
+
+    // ネクストの左端
+	json_["player_nxt1_x"] = { 479, 757 };
+    // ネクストの上端
     json_["player_nxt1_y"] = 109;
+
+    // ネクネクの左端
     json_["player_nxt2_x"] = { 514, 732 };
+    // ネクネクの上端
     json_["player_nxt2_y"] = 196;
-    json_["player_score_x"] = { 234, 833 };
-    json_["player_score_y"] = 589;
+
+    // スコア最上位0の左端
+	json_["player_score_x"] = { 234, 833 };
+    // スコア最上位0の上端
+	json_["player_score_y"] = 589;
 
     json_["game_debug_write"] = true;
     json_["game_debug_path"] = "D:/puyo/movie/image_debug";
